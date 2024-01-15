e4808adaea27348097f29dbf38b528bf3795d4f3
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2024-01-15 14:20:10 +0700

    Update bai4.js

--- ss24/bai4.js
+++ ss24/bai4.js
@@ -4,10 +4,12 @@ let tong=0,m;
 if(isNaN(n) || n<0){
     console.log("Số không hợp lệ");
 } else {
-    for(m=1;tong+m<n;m++){
-        if(m%2===0){
+    for(m=1;m<=n;m++){
+        tong = tong + m*m;
+        if(tong>n){
+            break;
+        } else if(m%2===0){
             console.log(`Bình phương ${m} = ${m*m}`);
-            tong = tong + m*m;
         }
     }
 }
